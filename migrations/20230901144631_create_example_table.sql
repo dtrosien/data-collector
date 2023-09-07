6e9ce710d0e4a70dde9e08e05d1a2a836f5071fc
dtrosien <dtrosien@gmail.com>
2023-09-07 18:35:24 +0200

    add postgres with sqlx ci

---
+++ migrations/20230901144631_create_example_table.sql
@@ -0,0 +1,10 @@
+-- Add migration script here
+-- Create Example Table
+CREATE TABLE example
+(
+    id            uuid        NOT NULL,
+    PRIMARY KEY (id),
+    email         TEXT        NOT NULL UNIQUE,
+    name          TEXT        NOT NULL,
+    created_at timestamptz NOT NULL
+);
