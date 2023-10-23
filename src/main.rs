9126006889c0735e7d78e2df445caa5d27c43e78
Telijas <telijas@googlemail.com>
2023-10-24 01:35:53 +0200

    Clean up module.

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,9 @@ mod nyse;
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let nyse_records = nyse::nyse::load_and_store_missing_data().await;
-    println!("Loaded {} records.", nyse_records.unwrap().len());
+    if let Ok(x) = nyse_records {
+        println!("Loaded {} records.", x.len());
+    }
     print!("done{}", "");
     Ok(())
 }
--- src/nyse/nyse.rs
+++ src/nyse/nyse.rs
@@ -1,15 +1,21 @@
+use std::error;
+
 use chrono::prelude::*;
 use chrono::{DateTime, Days, NaiveDate};
-use reqwest::{Client, Error};
+use reqwest::Client;
 use serde::{Deserialize, Serialize};
-use serde_json::error;
 
-use std::collections::HashMap;
+#[derive(Default, Deserialize, Serialize, Debug)]
+struct NyseRequest {
+    action_date__gte: NaiveDate,
+    action_date__lte: NaiveDate,
+    page: u32,
+    page_size: u32,
+}
 
 #[derive(Default, Deserialize, Debug)]
 struct NysePeekResponse {
     pub count: Option<u32>,
-    pub next: String,
 }
 
 #[derive(Default, Deserialize, Debug)]
@@ -31,57 +37,73 @@ pub struct NyseData {
     pub market_event: String,
 }
 
-#[derive(Default, Deserialize, Serialize, Debug)]
-struct NyseRequest {
-    action_date__gte: NaiveDate,
-    action_date__lte: NaiveDate,
-    page: u32,
-    page_size: u32,
-}
-
 impl NyseRequest {
     pub fn new(action_date__gte: NaiveDate, days: u64, page: u32, page_size: u32) -> Self {
         Self {
             action_date__gte,
             action_date__lte: action_date__gte
                 .checked_add_days(Days::new(days - 1))
-                .expect("Date should never leave the allowed values."),
+                .expect("Date should never leave the allowed range."),
             page,
             page_size,
         }
     }
 }
 
-fn latest_date_available() -> DateTime<Utc> {
-    //TODO: Get latest date from database
-    // let p = Utc.with_ymd_and_hms(2015, 12, 7, 0, 0, 0).unwrap();
-    let p = Utc.with_ymd_and_hms(2014, 10, 9, 0, 0, 0).unwrap();
-    p
-}
-
-pub async fn load_and_store_missing_data() -> Result<Vec<NyseData>, Box<dyn std::error::Error>> {
+pub async fn load_and_store_missing_data() -> Result<Vec<NyseData>, Box<dyn error::Error>> {
     let now = Utc::now();
     let mut latest_date = latest_date_available();
     let mut data: Vec<NyseData> = Vec::new();
+    let client = Client::new();
     while latest_date < now {
-        data.append(&mut load_missing_week(&latest_date).await?);
+        data.append(&mut load_missing_week(&client, &latest_date).await?);
         latest_date = latest_date
             .checked_add_days(Days::new(7))
-            .expect("Date should never leave the allowed values.");
+            .expect("Date should never leave the allowed range.");
     }
     Ok(data)
 }
 
-async fn load_missing_week(
+pub async fn load_missing_week(
+    client: &Client,
     date: &DateTime<Utc>,
-) -> Result<Vec<NyseData>, Box<dyn std::error::Error>> {
-    let client = Client::new();
-    let max_page_size = 100;
+) -> Result<Vec<NyseData>, Box<dyn error::Error>> {
+    let max_page_size = 100; //API does not allow more entries.
     let mut output: Vec<NyseData> = vec![];
 
-    let mut peak_response = match client
+    let peak_count = peek_number_results(&client, date).await?;
+
+    let pages_available: u32 = (peak_count as f32 / max_page_size as f32).ceil() as u32;
+    let list_of_pages: Vec<u32> = (1..=pages_available).collect();
+
+    for page in list_of_pages {
+        let response = request_nyse(&client, date, page, max_page_size).await?;
+        let mut response: NyseResponse = parse_nyse_response(response)?;
+
+        output.append(&mut response.results);
+    }
+    Ok(output)
+}
+
+async fn peek_number_results<'a>(
+    client: &Client,
+    date: &'a DateTime<Utc>,
+) -> Result<u32, Box<dyn error::Error>> {
+    let peak_response = request_nyse(client, date, 1, 1).await?;
+    let peek_response = parse_nyse_peek_response(peak_response)?;
+
+    Ok(peek_response.count.unwrap_or(0))
+}
+
+async fn request_nyse(
+    client: &Client,
+    date: &DateTime<Utc>,
+    page: u32,
+    max_page_size: u32,
+) -> Result<String, Box<dyn error::Error>> {
+    let response = match client
         .get("https://listingmanager.nyse.com/api/corpax/")
-        .query(&NyseRequest::new(date.date_naive(), 7, 1, 1))
+        .query(&NyseRequest::new(date.date_naive(), 7, page, max_page_size))
         .send()
         .await
     {
@@ -91,67 +113,43 @@ async fn load_missing_week(
                 "Error while loading data from NYSE (https://listingmanager.nyse.com/api/corpax/)."
             );
             if let Some(x) = error.url() {
-                println!("Issued query one page: {}", x);
+                println!("Error caused by query: {}", x);
             }
             return Err(Box::new(error));
         }
     }
     .text()
     .await?;
-    let mut peak_response: NysePeekResponse = match serde_json::from_str(&peak_response) {
+    Ok(response)
+}
+
+fn parse_nyse_peek_response(
+    peak_response: String,
+) -> Result<NysePeekResponse, Box<serde_json::Error>> {
+    let peak_response = match serde_json::from_str(&peak_response) {
         Ok(ok) => ok,
         Err(error) => {
             println!("Failed to parse response: {}", &peak_response);
             return Err(Box::new(error));
         }
     };
+    Ok(peak_response)
+}
 
-    // let mut peak_response: NysePeekResponse = match match client
-    //     .get("https://listingmanager.nyse.com/api/corpax/")
-    //     .query(&NyseRequest::new(date.date_naive(), 7, 1, 1))
-    //     .send()
-    //     .await
-    // {
-    //     Ok(ok) => ok,
-    //     Err(error) => {
-    //         println!(
-    //             "Error while loading data from NYSE (https://listingmanager.nyse.com/api/corpax/)."
-    //         );
-    //         if let Some(x) = error.url() {
-    //             println!("Issued query one page: {}", x);
-    //         }
-    //         return Err(Box::new(error));
-    //     }
-    // }
-    // .json()
-    // .await
-    // {
-    //     Ok(ok) => ok,
-    //     Err(error) => {
-    //         println!(
-    //             "Error while parsing json data from NYSE (https://listingmanager.nyse.com/api/corpax/).
-    //             Content: {}", error.to_string()
-    //         );
-
-    //         return Err(Box::new(error));
-    //     }
-    // };
-    let peak_count = peak_response.count.get_or_insert(0);
-
-    let pages_available: u32 = (*peak_count as f32 / max_page_size as f32).ceil() as u32;
-    let list_of_pages: Vec<u32> = (1..=pages_available).collect();
-    for page in list_of_pages {
-        let mut response = client
-            .get("https://listingmanager.nyse.com/api/corpax/")
-            // .query(&build_request(date, 7, max_page_size, page))
-            .query(&NyseRequest::new(date.date_naive(), 7, page, max_page_size))
-            .send()
-            .await?
-            .json::<NyseResponse>()
-            .await?;
-        if response.count > 0 {
-            output.append(&mut response.results);
+fn parse_nyse_response(peak_response: String) -> Result<NyseResponse, Box<serde_json::Error>> {
+    let peak_response = match serde_json::from_str(&peak_response) {
+        Ok(ok) => ok,
+        Err(error) => {
+            println!("Failed to parse response: {}", &peak_response);
+            return Err(Box::new(error));
         }
-    }
-    Ok(output)
+    };
+    Ok(peak_response)
+}
+
+fn latest_date_available() -> DateTime<Utc> {
+    //TODO: Get latest date from database
+    // let p = Utc.with_ymd_and_hms(2015, 12, 7, 0, 0, 0).unwrap();
+    let p = Utc.with_ymd_and_hms(2014, 10, 9, 0, 0, 0).unwrap();
+    p
 }
