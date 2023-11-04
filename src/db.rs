5e9048fd4edd948e08946a47af8de2e5695990c1
Telijas <telijas@googlemail.com>
2023-11-04 17:00:02 +0100

    More code cleanup

--- src/db.rs
+++ src/db.rs
@@ -5,7 +5,6 @@ use sqlx::{PgPool, Pool, Postgres};
 
 // use uuid::Uuid;
 
-
 pub fn create_connection_pool(configuration: &Settings) -> Pool<Postgres> {
     PgPoolOptions::new()
         .acquire_timeout(std::time::Duration::from_secs(2))
