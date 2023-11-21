d4e8893c0f9d694910e92a4d163fcd5c05d54807
Telijas <telijas@googlemail.com>
2023-11-21 01:18:24 +0100

    Introduce rusty-hook

--- src/main.rs
+++ src/main.rs
@@ -4,12 +4,8 @@ use data_collector::configuration::get_configuration;
 use data_collector::db;
 use data_collector::startup::run;
 use data_collector::utils::telemetry::{get_subscriber, init_subscriber};
+
 use std::error::Error;
-
-
-
-
-
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error + Send + Sync>> {
