a528e721d247e99e3a759cd76ae1951745ba110e
Telijas <telijas@googlemail.com>
2023-12-06 13:28:42 +0100

    Refactor of zip file reading and writing

--- src/collectors/source_apis/sec_companies.rs
+++ src/collectors/source_apis/sec_companies.rs
@@ -6,19 +6,20 @@ use async_trait::async_trait;
 use chrono::{DateTime, Days, Utc};
 use serde::Deserialize;
 use sqlx::PgPool;
-use std::io::{copy, Write};
+use std::io::{copy, Seek, Write};
 use std::{
     fmt::Display,
     fs::{self, File},
     io::Cursor,
     u8,
 };
+use zip::ZipArchive;
 
 use tracing::debug;
 
 use crate::tasks::runnable::Runnable;
 
-const download_target: &str = "https://www.rust-lang.org/logos/rust-logo-512x512.png";
+const DOWNLOAD_TARGET: &str = "https://www.rust-lang.org/logos/rust-logo-512x512.png";
 
 #[derive(Clone)]
 pub struct SecCompanyCollector {
@@ -91,7 +92,7 @@ impl Collector for SecCompanyCollector {
 }
 
 pub async fn load_and_store_missing_data(connection_pool: PgPool) -> Result<()> {
-    load_and_store_missing_data_with_target(connection_pool, download_target).await
+    load_and_store_missing_data_with_target(connection_pool, DOWNLOAD_TARGET).await
 }
 
 pub async fn load_and_store_missing_data_with_target(
@@ -106,38 +107,37 @@ pub async fn load_and_store_missing_data_with_target(
         download_url(url, target_location).await?;
     }
     let file = File::open(target_location)?;
-    let mut zip = zip::ZipArchive::new(file)?;
-    let mut counter = 0;
+    let mut zip = ZipArchive::new(file)?;
+
     let shrunk_File = File::create(target_location.clone().to_string() + ".tmp")?;
     let mut new_zip = zip::ZipWriter::new(shrunk_File);
     let mut data: Vec<SecCompany> = vec![];
-    for i in 0..zip.len() {
-        let mut file = zip.by_index(i)?;
-        if !(file.name().contains("submission") || file.name().contains("placeholder.txt")) {
-            let mut buffer: Vec<u8> = Vec::new();
-            let mut cursor = Cursor::new(&mut buffer);
-            std::io::copy(&mut file, &mut cursor);
-            let v = cursor.into_inner();
-            let output = String::from_utf8_lossy(v).to_string();
-            let infos: SecCompany = collectors::utils::parse_response(&output)?;
-            if infos.exchanges.len() > 0 || infos.tickers.len() > 0 {
-                counter += 1;
-                let _options = zip::write::FileOptions::default()
-                    .compression_method(zip::CompressionMethod::Stored);
-                new_zip.raw_copy_file(file)?;
+    let num = &zip.len();
+    for i in 0..*num {
+        // Check if file is needed
+        let mut is_file_needed = false;
+        {
+            let mut file = zip.by_index(i)?;
+            if !(file.name().contains("submission") || file.name().contains("placeholder.txt")) {
+                let mut buffer: Vec<u8> = Vec::new();
+                let mut cursor = Cursor::new(&mut buffer);
+                std::io::copy(&mut file, &mut cursor)?;
+
+                let v = cursor.into_inner();
+                let output = String::from_utf8_lossy(v).to_string();
+                let infos: SecCompany = collectors::utils::parse_response(&output)?;
+                is_file_needed = infos.exchanges.len() > 0 || infos.tickers.len() > 0;
                 data.push(infos);
-                // println!("Found: {:?}", infos);
             }
+        }
+        if is_file_needed {
+            new_zip.raw_copy_file(zip.by_index(i)?)?;
         }
     }
     println!("Calling finsih");
     new_zip.finish()?;
+
     println!("Finsih called");
-    println!(
-        "Stock companies found {} out of total {}",
-        counter,
-        zip.len()
-    );
     let transposed_data = transpose_sec_companies(data);
 
     sqlx::query!("INSERT INTO sec_companies (cik, sic, \"name\", ticker, exchange, state_of_incorporation) Select * from UNNEST ($1::int4[],$2::int[],$3::text[],$4::text[],$5::text[],$6::text[]) on conflict do nothing",
@@ -190,7 +190,7 @@ fn transpose_sec_companies(companies: Vec<SecCompany>) -> TransposedSecCompany {
 mod test {
     use crate::{
         collectors::source_apis::sec_companies::{
-            download_target, load_and_store_missing_data_with_target,
+            load_and_store_missing_data_with_target, DOWNLOAD_TARGET,
         },
         configuration::get_configuration,
         utils::errors::Result,
@@ -201,13 +201,12 @@ mod test {
     #[traced_test]
     #[sqlx::test]
     async fn query_http_and_write_to_db(pool: Pool<Postgres>) -> Result<()> {
-        let mut configuration = get_configuration().expect("Failed to read configuration.");
-        // let connection_pool = configure_database(&configuration.database).await;
-        let connection_pool = PgPool::connect_with(configuration.database.with_db())
-            .await
-            .expect("Failed to connect to Postgres.");
-        // load_and_store_missing_data_with_target(pool.clone(), download_target).await?;
-        load_and_store_missing_data_with_target(connection_pool, download_target).await?;
+        // let configuration = get_configuration().expect("Failed to read configuration.");
+        // let connection_pool = PgPool::connect_with(configuration.database.with_db())
+        //     .await
+        //     .expect("Failed to connect to Postgres.");
+        // load_and_store_missing_data_with_target(connection_pool, download_target).await?;
+        load_and_store_missing_data_with_target(pool.clone(), DOWNLOAD_TARGET).await?;
         Ok(())
     }
 
