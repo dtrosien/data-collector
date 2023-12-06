938950630e3fbe924eb6f121ff1ffef5c91c3960
Telijas <telijas@googlemail.com>
2023-12-06 01:49:00 +0100

    First draft of SEC company loading

--- migrations/20230901144631_create_example_table.sql
+++ migrations/20230901144631_create_example_table.sql
@@ -46,6 +46,23 @@ cluster verbose NYSE_INSTRUMENTS using staged_instruments;
 analyse verbose NYSE_INSTRUMENTS;
 
 
+CREATE TABLE SEC_COMPANIES (
+    cik INT NOT NULL,
+    sic INT,
+    name VARCHAR(200) NOT NULL,
+    ticker VARCHAR(10) NOT NULL,
+    exchange VARCHAR(10),
+    state_of_incorporation VARCHAR(2) NOT NULL,
+    date_loaded DATE DEFAULT CURRENT_DATE NOT NULL,
+    is_staged BOOLEAN DEFAULT FALSE NOT NULL,
+    PRIMARY KEY (name, ticker)
+);
+create index staged_sec_companies on SEC_COMPANIES using btree (is_staged desc);
+
+cluster verbose SEC_COMPANIES using staged_sec_companies;
+analyse verbose SEC_COMPANIES;
+
+
 CREATE TABLE micCodes (
     Market_Name VARCHAR(255) NOT NULL,
     Mic_Code CHAR(4) NOT NULL,
--- src/collectors/source_apis/sec_companies.rs
+++ src/collectors/source_apis/sec_companies.rs
@@ -3,20 +3,17 @@ use crate::{
     utils::errors::Result,
 };
 use async_trait::async_trait;
-use chrono::{DateTime, Days, Duration, Utc};
+use chrono::{DateTime, Days, Utc};
 use serde::Deserialize;
 use sqlx::PgPool;
+use std::io::{copy, Write};
 use std::{
     fmt::Display,
     fs::{self, File},
-    io::{Bytes, Cursor},
-    ops::Add,
-    path::Path,
-    time::SystemTime,
+    io::Cursor,
     u8,
 };
-use std::{fs::rename, io::copy};
-use tempfile::Builder;
+
 use tracing::debug;
 
 use crate::tasks::runnable::Runnable;
@@ -29,9 +26,39 @@ pub struct SecCompanyCollector {
 }
 
 #[derive(Default, Deserialize, Debug, PartialEq)]
-pub struct exchanges {
+#[serde(rename_all = "camelCase")]
+pub struct SecCompany {
+    cik: String,
+    sic: String,
+    name: String,
     tickers: Vec<Option<String>>,
     exchanges: Vec<Option<String>>,
+    state_of_incorporation: String,
+}
+
+#[derive(Default, Deserialize, Debug, PartialEq)]
+#[serde(rename_all = "camelCase")]
+pub struct TransposedSecCompany {
+    pub cik: Vec<i32>,
+    pub sic: Vec<Option<i32>>,
+    pub name: Vec<String>,
+    pub tickers: Vec<String>,
+    pub exchanges: Vec<Option<String>>,
+    pub state_of_incorporation: Vec<String>,
+}
+
+impl TransposedSecCompany {
+    ///Creates empty object
+    fn new() -> TransposedSecCompany {
+        TransposedSecCompany {
+            cik: vec![],
+            sic: vec![],
+            name: vec![],
+            tickers: vec![],
+            exchanges: vec![],
+            state_of_incorporation: vec![],
+        }
+    }
 }
 
 impl SecCompanyCollector {
@@ -80,22 +107,47 @@ pub async fn load_and_store_missing_data_with_target(
     }
     let file = File::open(target_location)?;
     let mut zip = zip::ZipArchive::new(file)?;
-    for i in 0..10000 {
+    let mut counter = 0;
+    let shrunk_File = File::create(target_location.clone().to_string() + ".tmp")?;
+    let mut new_zip = zip::ZipWriter::new(shrunk_File);
+    let mut data: Vec<SecCompany> = vec![];
+    for i in 0..zip.len() {
         let mut file = zip.by_index(i)?;
-        if !file.name().contains("submission") {
+        if !(file.name().contains("submission") || file.name().contains("placeholder.txt")) {
             let mut buffer: Vec<u8> = Vec::new();
             let mut cursor = Cursor::new(&mut buffer);
             std::io::copy(&mut file, &mut cursor);
-            let v = &cursor.into_inner();
+            let v = cursor.into_inner();
             let output = String::from_utf8_lossy(v).to_string();
-            // println!("output {}", output);
-            let infos: exchanges = collectors::utils::parse_response(&output)?;
-            if infos.exchanges.len() > 0 {
-                println!("Filename: {}", file.name());
-                println!("Found: {:?}", infos);
+            let infos: SecCompany = collectors::utils::parse_response(&output)?;
+            if infos.exchanges.len() > 0 || infos.tickers.len() > 0 {
+                counter += 1;
+                let _options = zip::write::FileOptions::default()
+                    .compression_method(zip::CompressionMethod::Stored);
+                new_zip.raw_copy_file(file)?;
+                data.push(infos);
+                // println!("Found: {:?}", infos);
             }
         }
     }
+    println!("Calling finsih");
+    new_zip.finish()?;
+    println!("Finsih called");
+    println!(
+        "Stock companies found {} out of total {}",
+        counter,
+        zip.len()
+    );
+    let transposed_data = transpose_sec_companies(data);
+
+    sqlx::query!("INSERT INTO sec_companies (cik, sic, \"name\", ticker, exchange, state_of_incorporation) Select * from UNNEST ($1::int4[],$2::int[],$3::text[],$4::text[],$5::text[],$6::text[]) on conflict do nothing",
+    &transposed_data.cik[..],
+    &transposed_data.sic[..] as _,
+    &transposed_data.name[..],
+    &transposed_data.tickers[..],
+    &transposed_data.exchanges[..] as _,
+    &transposed_data.state_of_incorporation[..])
+    .execute(&connection_pool).await?;
 
     Ok(())
 }
@@ -108,25 +160,60 @@ async fn download_url(url: &str, destination: &str) -> Result<()> {
     Ok(())
 }
 
+fn transpose_sec_companies(companies: Vec<SecCompany>) -> TransposedSecCompany {
+    let mut result = TransposedSecCompany::new();
+    for company in companies {
+        for i in 0..company.tickers.len() {
+            result.cik.push(
+                company
+                    .cik
+                    .parse::<i32>()
+                    .expect("Only numbers expected here."),
+            );
+            result.sic.push(company.sic.parse::<i32>().ok());
+            result.name.push(company.name.clone());
+            result.tickers.push(
+                company.tickers[i]
+                    .to_owned()
+                    .expect("Iterating over available tickers."),
+            );
+            result.exchanges.push(company.exchanges[i].to_owned());
+            result
+                .state_of_incorporation
+                .push(company.state_of_incorporation.clone());
+        }
+    }
+    result
+}
+
 #[cfg(test)]
 mod test {
     use crate::{
         collectors::source_apis::sec_companies::{
             download_target, load_and_store_missing_data_with_target,
         },
+        configuration::get_configuration,
         utils::errors::Result,
     };
-    use sqlx::{Pool, Postgres};
+    use sqlx::{PgPool, Pool, Postgres};
     use tracing_test::traced_test;
 
     #[traced_test]
     #[sqlx::test]
     async fn query_http_and_write_to_db(pool: Pool<Postgres>) -> Result<()> {
-        load_and_store_missing_data_with_target(pool.clone(), download_target).await?;
+        let mut configuration = get_configuration().expect("Failed to read configuration.");
+        // let connection_pool = configure_database(&configuration.database).await;
+        let connection_pool = PgPool::connect_with(configuration.database.with_db())
+            .await
+            .expect("Failed to connect to Postgres.");
+        // load_and_store_missing_data_with_target(pool.clone(), download_target).await?;
+        load_and_store_missing_data_with_target(connection_pool, download_target).await?;
         Ok(())
     }
 
     fn outdated_time_is_correctly_detected() {}
 
     fn file_is_only_loaded_when_outdated() {}
+
+    fn file_smaller_10MB_will_be_skipped() {}
 }
