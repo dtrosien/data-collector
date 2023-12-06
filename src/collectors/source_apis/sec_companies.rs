71a55e851bce7a9d68fdafdba75e54dbac8f96b7
Telijas <telijas@googlemail.com>
2023-12-06 20:55:27 +0100

    Refactor code

--- src/collectors/source_apis/sec_companies.rs
+++ src/collectors/source_apis/sec_companies.rs
@@ -6,11 +6,12 @@ use async_trait::async_trait;
 use chrono::{DateTime, Days, Utc};
 use serde::Deserialize;
 use sqlx::PgPool;
-use std::io::{copy, Seek, Write};
+use std::io::copy;
 use std::{
     fmt::Display,
     fs::{self, File},
     io::Cursor,
+    path::PathBuf,
     u8,
 };
 use zip::ZipArchive;
@@ -19,7 +20,10 @@ use tracing::debug;
 
 use crate::tasks::runnable::Runnable;
 
-const DOWNLOAD_TARGET: &str = "https://www.rust-lang.org/logos/rust-logo-512x512.png";
+const DOWNLOAD_SOURCE: &str = "https://www.rust-lang.org/logos/rust-logo-512x512.png";
+const TARGET_SUBDIRECTORIES: &str = "data-collector/sec_companies";
+const TARGET_FILE_NAME: &str = "submissions.zip";
+const TARGET_TMP_FILE_NAME: &str = "submissions.zip.tmp";
 
 #[derive(Clone)]
 pub struct SecCompanyCollector {
@@ -92,64 +96,113 @@ impl Collector for SecCompanyCollector {
 }
 
 pub async fn load_and_store_missing_data(connection_pool: PgPool) -> Result<()> {
-    load_and_store_missing_data_with_target(connection_pool, DOWNLOAD_TARGET).await
+    load_and_store_missing_data_with_target(connection_pool, DOWNLOAD_SOURCE).await
 }
 
 pub async fn load_and_store_missing_data_with_target(
     connection_pool: PgPool,
     url: &str,
 ) -> Result<()> {
-    let target_location = "/home/telijas/submissions.zip";
-    let metadata = fs::metadata(target_location)?;
-    let modification_date: DateTime<Utc> = metadata.modified().unwrap().into();
-    if modification_date.checked_add_days(Days::new(7)).unwrap() < Utc::now() {
-        debug!("Downloading {}", url);
-        download_url(url, target_location).await?;
-    }
-    let file = File::open(target_location)?;
-    let mut zip = ZipArchive::new(file)?;
+    let zip_file_location = prepare_generic_zip_location(TARGET_FILE_NAME)?;
+    download_archive_if_needed(&zip_file_location, url).await?;
+    let zip_archive = get_source_zip_file(&zip_file_location)?;
 
-    let shrunk_File = File::create(target_location.clone().to_string() + ".tmp")?;
-    let mut new_zip = zip::ZipWriter::new(shrunk_File);
-    let mut data: Vec<SecCompany> = vec![];
-    let num = &zip.len();
-    for i in 0..*num {
-        // Check if file is needed
-        let mut is_file_needed = false;
-        {
-            let mut file = zip.by_index(i)?;
-            if !(file.name().contains("submission") || file.name().contains("placeholder.txt")) {
-                let mut buffer: Vec<u8> = Vec::new();
-                let mut cursor = Cursor::new(&mut buffer);
-                std::io::copy(&mut file, &mut cursor)?;
-
-                let v = cursor.into_inner();
-                let output = String::from_utf8_lossy(v).to_string();
-                let infos: SecCompany = collectors::utils::parse_response(&output)?;
-                is_file_needed = infos.exchanges.len() > 0 || infos.tickers.len() > 0;
-                data.push(infos);
-            }
-        }
-        if is_file_needed {
-            new_zip.raw_copy_file(zip.by_index(i)?)?;
-        }
-    }
-    println!("Calling finsih");
-    new_zip.finish()?;
-
-    println!("Finsih called");
-    let transposed_data = transpose_sec_companies(data);
+    let found_data = search_and_shrink_zip(zip_archive, zip_file_location)?;
+    let transposed_data = transpose_sec_companies(found_data);
 
     sqlx::query!("INSERT INTO sec_companies (cik, sic, \"name\", ticker, exchange, state_of_incorporation) Select * from UNNEST ($1::int4[],$2::int[],$3::text[],$4::text[],$5::text[],$6::text[]) on conflict do nothing",
     &transposed_data.cik[..],
-    &transposed_data.sic[..] as _,
+    &transposed_data.sic[..] as _, //cast due to None's in the vector
     &transposed_data.name[..],
     &transposed_data.tickers[..],
-    &transposed_data.exchanges[..] as _,
+    &transposed_data.exchanges[..] as _, //cast due to None's in the vector
     &transposed_data.state_of_incorporation[..])
     .execute(&connection_pool).await?;
 
     Ok(())
+}
+
+fn search_and_shrink_zip(
+    mut zip_archive: ZipArchive<File>,
+    target_location: PathBuf,
+) -> Result<Vec<SecCompany>> {
+    let tmp_location = compute_tmp_location(&target_location);
+    let mut new_zip = zip::ZipWriter::new(File::create(tmp_location.clone())?);
+    let mut found_data: Vec<SecCompany> = vec![];
+    for i in 0..zip_archive.len() {
+        let mut buffer: Vec<u8> = Vec::new();
+        let mut cursor = Cursor::new(&mut buffer);
+        {
+            let mut file = zip_archive.by_index(i)?;
+            if file.name().contains("submission") || file.name().contains("placeholder.txt") {
+                continue;
+            }
+            std::io::copy(&mut file, &mut cursor)?;
+        }
+        let output = String::from_utf8_lossy(cursor.into_inner()).to_string();
+        let infos: SecCompany = collectors::utils::parse_response(&output)?;
+        if infos.exchanges.len() > 0 || infos.tickers.len() > 0 {
+            found_data.push(infos);
+            new_zip.raw_copy_file(zip_archive.by_index(i)?)?;
+        }
+    }
+    new_zip.finish()?;
+    fs::rename(tmp_location, target_location)?;
+    Ok(found_data)
+}
+
+fn compute_tmp_location(target_location: &PathBuf) -> PathBuf {
+    let mut tmp_location = target_location.clone();
+    tmp_location.pop();
+    tmp_location.push(TARGET_TMP_FILE_NAME);
+    tmp_location
+}
+
+fn get_source_zip_file(target_location: &PathBuf) -> Result<ZipArchive<File>> {
+    let file = File::open(target_location.to_str().unwrap())?;
+    let zip_archive = ZipArchive::new(file)?;
+    Ok(zip_archive)
+}
+
+async fn download_archive_if_needed(target_location: &PathBuf, url: &str) -> Result<()> {
+    let is_update_needed = match fs::metadata(target_location) {
+        Ok(metadata) => {
+            let modification_date: DateTime<Utc> = metadata.modified().unwrap().into();
+            modification_date.checked_add_days(Days::new(7)).unwrap() < Utc::now()
+        }
+        Err(_) => true,
+    };
+
+    Ok(if is_update_needed {
+        debug!("Downloading {}", url);
+        download_url(url, target_location.to_str().unwrap()).await?;
+    })
+}
+
+/// Creates directories if needed and return the location to the zip file, independent, if it is existing or not.
+fn prepare_generic_zip_location(filename: &str) -> Result<std::path::PathBuf> {
+    prepare_zip_location(
+        home::home_dir().unwrap().to_str().unwrap(),
+        TARGET_SUBDIRECTORIES,
+        filename,
+    )
+}
+
+/// Creates directories if needed and return the location to the zip file, independent, if it is existing or not.
+fn prepare_zip_location(
+    root_path: &str,
+    intermediate_path: &str,
+    file_name: &str,
+) -> Result<std::path::PathBuf> {
+    let mut path_buf = PathBuf::from(root_path);
+    path_buf.push(intermediate_path);
+    fs::create_dir_all(
+        path_buf
+            .to_str()
+            .ok_or("Invalid character for path to SEC zip file.")?,
+    )?;
+    path_buf.push(file_name);
+    Ok(path_buf)
 }
 
 async fn download_url(url: &str, destination: &str) -> Result<()> {
@@ -190,12 +243,11 @@ fn transpose_sec_companies(companies: Vec<SecCompany>) -> TransposedSecCompany {
 mod test {
     use crate::{
         collectors::source_apis::sec_companies::{
-            load_and_store_missing_data_with_target, DOWNLOAD_TARGET,
+            load_and_store_missing_data_with_target, DOWNLOAD_SOURCE,
         },
-        configuration::get_configuration,
         utils::errors::Result,
     };
-    use sqlx::{PgPool, Pool, Postgres};
+    use sqlx::{Pool, Postgres};
     use tracing_test::traced_test;
 
     #[traced_test]
@@ -206,7 +258,7 @@ mod test {
         //     .await
         //     .expect("Failed to connect to Postgres.");
         // load_and_store_missing_data_with_target(connection_pool, download_target).await?;
-        load_and_store_missing_data_with_target(pool.clone(), DOWNLOAD_TARGET).await?;
+        load_and_store_missing_data_with_target(pool.clone(), DOWNLOAD_SOURCE).await?;
         Ok(())
     }
 
@@ -214,5 +266,9 @@ mod test {
 
     fn file_is_only_loaded_when_outdated() {}
 
-    fn file_smaller_10MB_will_be_skipped() {}
+    fn file_smaller_10mb_will_be_skipped() {}
+
+    fn read_write_zip_and_reread_again() {}
+
+    fn check_if_zip_location_is_created() {}
 }
