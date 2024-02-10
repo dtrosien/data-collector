fe336372038bf39ce42fb5d1c6620eb4e2f4b252
Telijas <telijas@googlemail.com>
2024-02-10 23:05:47 +0100

    Update master data table, introduce instuments, update nyse_instrument staging and fix tests in sec_companies

--- tests/resources/collectors/staging/nyse_instruments_staging/master_data_with_company.sql
+++ tests/resources/collectors/staging/nyse_instruments_staging/master_data_with_company.sql
@@ -1,6 +1,6 @@
 --2  Lines
-INSERT INTO master_data (issuer_name, issue_symbol, "location", start_nyse, start_nyse_arca, start_nyse_american, start_nasdaq, start_nasdaq_global_select_market, start_nasdaq_select_market, start_nasdaq_capital_market, is_company, category, renamed_to_issuer_name, renamed_to_issue_symbol, renamed_at_date, current_name, suspended, suspension_date, start_cboe) VALUES 
+INSERT INTO master_data (issuer_name, issue_symbol, "location", start_nyse, start_nyse_arca, start_nyse_american, start_nasdaq, start_nasdaq_global_select_market, start_nasdaq_select_market, start_nasdaq_capital_market, instrument, renamed_to_issuer_name, renamed_to_issue_symbol, renamed_at_date, current_name, suspended, suspension_date, start_cboe) VALUES 
 ('BANK OF HAWAII CORP', 'BOH', 'USA', '2024-01-15', NULL, NULL, NULL, NULL, NULL, NULL, true, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL),
-('Aurora Technology Acquisition Corp.', 'ATAKR', 'CYM', NULL, NULL, NULL, NULL, NULL, '2024-01-15', NULL, false, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL);
+('Aurora Technology Acquisition Corp.', 'ATAKR', 'CYM', NULL, NULL, NULL, NULL, NULL, '2024-01-15', NULL,  NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL);
 ;
 
