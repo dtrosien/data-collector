2622586e75a1ffee5b6ba4d14cf02ca6b054d1ea
Telijas <telijas@googlemail.com>
2024-10-09 15:01:09 +0200

    Add structs for market data preparation

--- src/actions/stage/polygon_grouped_daily.rs
+++ src/actions/stage/polygon_grouped_daily.rs
@@ -1,11 +1,151 @@
 use async_trait::async_trait;
-use futures_util::TryFutureExt;
+use chrono::NaiveDate;
+use futures_util::{TryFutureExt, TryStreamExt};
 
+use rand::Error;
+use sqlx::types::BigDecimal;
 use sqlx::PgPool;
+// use tokio_stream::StreamExt;
+use futures_util::{pin_mut, stream, Stream, StreamExt};
 use std::fmt::Display;
+use tracing::info;
 
 use crate::dag_schedule::task::TaskError::UnexpectedError;
 use crate::dag_schedule::task::{Runnable, StatsMap, TaskError};
+
+#[derive(Clone, Debug)]
+pub struct market_data_transposed {
+    symbol: Vec<String>,
+    business_date: Vec<NaiveDate>,
+    year_month: Vec<Option<u16>>,
+    stock_price: Vec<BigDecimal>,
+    open: Vec<Option<BigDecimal>>,
+    close: Vec<Option<BigDecimal>>,
+    volume_trade: Vec<Option<BigDecimal>>,
+    shares_traded: Vec<Option<i32>>,
+    after_hours: Vec<Option<BigDecimal>>,
+    pre_market: Vec<Option<BigDecimal>>,
+    market_capitalization: Vec<Option<BigDecimal>>,
+}
+
+#[derive(Clone, Debug)]
+pub struct market_data {
+    symbol: String,
+    business_date: NaiveDate,
+    year_month: Option<u16>,
+    stock_price: BigDecimal,
+    open: Option<BigDecimal>,
+    close: Option<BigDecimal>,
+    volume_trade: Option<BigDecimal>,
+    shares_traded: Option<i32>,
+    after_hours: Option<BigDecimal>,
+    pre_market: Option<BigDecimal>,
+    market_capitalization: Option<BigDecimal>,
+}
+
+#[derive(Clone, Debug)]
+pub struct market_data_builder {
+    symbol: Option<String>,
+    business_date: Option<NaiveDate>,
+    year_month: Option<u16>,
+    stock_price: Option<BigDecimal>,
+    open: Option<BigDecimal>,
+    close: Option<BigDecimal>,
+    volume_trade: Option<BigDecimal>,
+    shares_traded: Option<i32>,
+    after_hours: Option<BigDecimal>,
+    pre_market: Option<BigDecimal>,
+    market_capitalization: Option<BigDecimal>,
+}
+
+impl market_data_builder {
+    fn builder() -> market_data_builder {
+        market_data_builder {
+            symbol: None,
+            business_date: None,
+            year_month: None,
+            stock_price: None,
+            open: None,
+            close: None,
+            volume_trade: None,
+            shares_traded: None,
+            after_hours: None,
+            pre_market: None,
+            market_capitalization: None,
+        }
+    }
+
+    fn symbol(mut self, symbol: String) -> Self {
+        self.symbol = Some(symbol);
+        self
+    }
+
+    fn business_date(mut self, business_date: NaiveDate) -> Self {
+        self.business_date = Some(business_date);
+        self
+    }
+
+    fn year_month(mut self, year_month: Option<u16>) -> Self {
+        self.year_month = year_month;
+        self
+    }
+
+    fn stock_price(mut self, stock_price: BigDecimal) -> Self {
+        self.stock_price = Some(stock_price);
+        self
+    }
+
+    fn open(mut self, open: Option<BigDecimal>) -> Self {
+        self.open = open;
+        self
+    }
+
+    fn close(mut self, close: Option<BigDecimal>) -> Self {
+        self.close = close;
+        self
+    }
+
+    fn volume_trade(mut self, volume_trade: Option<BigDecimal>) -> Self {
+        self.volume_trade = volume_trade;
+        self
+    }
+
+    fn shares_traded(mut self, shares_traded: Option<i32>) -> Self {
+        self.shares_traded = shares_traded;
+        self
+    }
+    fn after_hours(mut self, after_hours: Option<BigDecimal>) -> Self {
+        self.after_hours = after_hours;
+        self
+    }
+    fn pre_market(mut self, pre_market: Option<BigDecimal>) -> Self {
+        self.pre_market = pre_market;
+        self
+    }
+    fn market_capitalization(mut self, market_capitalization: Option<BigDecimal>) -> Self {
+        self.market_capitalization = market_capitalization;
+        self
+    }
+
+    fn build(self) -> Result<market_data, Error> {
+        if self.symbol.is_some() && self.business_date.is_some() && self.stock_price.is_some() {
+            return Ok(market_data {
+                symbol: self.symbol.expect("Checked earlier"),
+                business_date: self.business_date.expect("Checked earlier"),
+                year_month: self.year_month,
+                stock_price: self.stock_price.expect("Checked earlier"),
+                open: self.open,
+                close: self.close,
+                volume_trade: self.volume_trade,
+                shares_traded: self.shares_traded,
+                after_hours: self.after_hours,
+                pre_market: self.pre_market,
+                market_capitalization: self.market_capitalization,
+            });
+        }
+        Err(Error::new("err"))
+    }
+}
 
 #[derive(Clone, Debug)]
 pub struct PolygonGroupedDailyStager {
@@ -38,7 +178,7 @@ impl Runnable for PolygonGroupedDailyStager {
 #[tracing::instrument(level = "debug", skip_all)]
 pub async fn stage_data(connection_pool: PgPool) -> Result<(), anyhow::Error> {
     let partitions = get_existing_partitions(&connection_pool).await?;
-
+    get_stageable_data(&connection_pool).await?;
     // stage_nulls(&connection_pool).await?;
 
     //Copy initial public offering dates to master data
@@ -47,6 +187,38 @@ pub async fn stage_data(connection_pool: PgPool) -> Result<(), anyhow::Error> {
     //Mark as staged in
     // mark_ipo_date_as_staged(&connection_pool).await?;
     Ok(())
+}
+
+// select pgd.symbol, pgd."open", pgd."close", pgd.business_date,  pgd.stock_volume, pgd.traded_volume from polygon_grouped_daily pgd where pgd.is_staged = false;
+
+#[tracing::instrument(level = "debug", skip_all)]
+async fn get_stageable_data(connection_pool: &PgPool) -> Result<Vec<u32>, anyhow::Error> {
+    let mut a = sqlx::query!(
+        r##"select pgd.symbol, pgd."open", pgd."close", pgd.business_date, pgd.stock_volume, pgd.traded_volume from polygon_grouped_daily pgd where pgd.is_staged = false limit 30"##).fetch(connection_pool);
+    while let Some(batch) = a.as_mut().chunks(10).next().await {
+        let f: Vec<market_data> = batch
+            .into_iter()
+            .filter(|x| x.is_ok())
+            .map(|x| {
+                let a = x.expect("Checked before");
+                return market_data_builder::builder()
+                    .symbol(a.symbol)
+                    .stock_price(a.close.clone())
+                    .open(Some(a.open))
+                    .close(Some(a.close))
+                    .business_date(a.business_date)
+                    .shares_traded(a.stock_volume)
+                    .volume_trade(Some(a.traded_volume))
+                    .build()
+                    .unwrap();
+            })
+            .collect();
+
+        // foo(batch).await; // Pass each chunk of rows to foo
+        println!("Batch result: {:?}", f);
+    }
+
+    Ok(vec![])
 }
 
 #[tracing::instrument(level = "debug", skip_all)]
@@ -74,6 +246,7 @@ async fn get_existing_partition_ranges_for_oid(
     table_oid: u32,
 ) -> Result<Vec<u32>, anyhow::Error> {
     let help_int: i64 = table_oid.into();
+    info!("table id: {}", help_int);
     // Returns strings of type "FOR VALUES IN (200002)" or "DEFAULT"
     let result = sqlx::query!(
         "SELECT
