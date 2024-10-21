ca9b02ee22341ad2390f5a88958db99f77f258aa
Telijas <telijas@googlemail.com>
2024-10-21 21:08:13 +0200

    Add staging of partial entries and tests

---
+++ tests/resources/collectors/staging/polygon_grouped_daily_staging/market_data_update_entries.sql
@@ -0,0 +1,12 @@
+INSERT INTO market_data
+(symbol, business_date, year_month, stock_price, "open", "close", stock_traded, order_amount, after_hours, pre_market, market_capitalization)
+VALUES('A', '2022-03-04', 202203, 133.89, 1.0, 133.89, 3848511.0, 48479.0, NULL, NULL, NULL);
+INSERT INTO market_data
+(symbol, business_date, year_month, stock_price, "open", "close", stock_traded, order_amount, after_hours, pre_market, market_capitalization)
+VALUES('A', '2022-03-07', 202203, 130.34, 133.55, 130.34, 2376504.0, NULL, NULL, NULL, NULL);
+INSERT INTO market_data
+(symbol, business_date, year_month, stock_price, "open", "close", stock_traded, order_amount, after_hours, pre_market, market_capitalization)
+VALUES('A', '2022-03-08', 202203, 130.34, 133.55, 130.34, 2376504.0, NULL, NULL, NULL, NULL);
+INSERT INTO market_data
+(symbol, business_date, year_month, stock_price, "open", "close", stock_traded, order_amount, after_hours, pre_market, market_capitalization)
+VALUES('A', '2022-03-09', 202203, 130.34, 133.55, 130.34, 2376504.0, 10.0, NULL, NULL, NULL);
---
+++ tests/resources/collectors/staging/polygon_grouped_daily_staging/polygon_grouped_daily_data_source.sql
@@ -0,0 +1,13 @@
+INSERT INTO polygon_grouped_daily
+("close", business_date, high, low, "open", symbol, order_amount, stock_traded, volume_weighted_average_price, is_staged)
+VALUES(133.8900, '2022-03-04', 137.1200, 132.1400, 135.8500, 'A', 48479, 3848511.0000, 134.2259, false);
+INSERT INTO polygon_grouped_daily
+("close", business_date, high, low, "open", symbol, order_amount, stock_traded, volume_weighted_average_price, is_staged)
+VALUES(130.3400, '2022-03-07', 133.5500, 128.4300, 133.5500, 'A', 36120, 2376504.0000, 130.3950, false);
+INSERT INTO polygon_grouped_daily
+("close", business_date, high, low, "open", symbol, order_amount, stock_traded, volume_weighted_average_price, is_staged)
+VALUES(130.3400, '2022-03-08', 133.5500, 128.4300, 133.5500, 'A', NULL, 2376504.0000, 130.3950, false);
+INSERT INTO polygon_grouped_daily
+("close", business_date, high, low, "open", symbol, order_amount, stock_traded, volume_weighted_average_price, is_staged)
+VALUES(130.3400, '2022-03-09', 133.5500, 128.4300, 133.5500, 'A', NULL, 2376504.0000, 130.3950, false);
+
