7b91a259053ddbdaa3a03be53354c6ab86abd9b9
Telijas <telijas@googlemail.com>
2026-07-11 22:27:26 +0200

    Listen to newer clippy

--- src/actions/collect/polygon_dividends.rs
+++ src/actions/collect/polygon_dividends.rs
@@ -47,7 +47,7 @@ impl PolygonDividendsRequest<'_> {
 
 impl Display for PolygonDividendsRequest<'_> {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(f, "{}", &self.base)?;
+        write!(f, "{}", self.base)?;
         Secret::new(self.api_key.expose_secret_for_data_structure().clone()).fmt(f)
     }
 }
--- src/actions/collect/polygon_open_close.rs
+++ src/actions/collect/polygon_open_close.rs
@@ -44,7 +44,7 @@ impl PolygonOpenCloseRequest<'_> {
 
 impl Display for PolygonOpenCloseRequest<'_> {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(f, "{}", &self.base)?;
+        write!(f, "{}", self.base)?;
         Secret::new(self.api_key.expose_secret_for_data_structure().clone()).fmt(f)
     }
 }
